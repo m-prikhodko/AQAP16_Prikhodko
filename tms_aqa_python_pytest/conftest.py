9952f121a7823241ce14c86f60019f5379d4177e
m-prikhodko <m.prikhodko@markformelle.by>
2024-02-03 15:22:20 +0300

    fixed 1-2 comments

--- tms_aqa_python_pytest/conftest.py
+++ tms_aqa_python_pytest/conftest.py
@@ -1,7 +1,4 @@
-import time
-
 import pytest
-from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
 
